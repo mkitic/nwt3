714ddaed8a82f11ccc044d7e31aa6298612168a5
kula <kula124@gmail.com>
2020-09-21 12:14:49 +0200

    img

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -6,7 +6,7 @@ const ImageCard=({image})=> {
 
     return(
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <image src={image.webformatURL} alt="" className="w-full"/>
+      <img src={image.webformatURL} alt="" className="w-full"/>
       <div className="px-6 py-4">
           <div className="font-bold text-purple-500 text-xl mb-2">
               Photo by {image.user}
